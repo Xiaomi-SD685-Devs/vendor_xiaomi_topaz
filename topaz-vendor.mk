5cc49c5d1975ebee82d5a97dd5f353f287bd86cb
boedhack99 <boedhack@gmail.com>
2023-12-17 13:45:12 +0000

    topaz: Import RIL patched from device tree * from marble MIUI V14.0.9.0.TRMCNXM

--- topaz-vendor.mk
+++ topaz-vendor.mk
@@ -1681,11 +1681,13 @@ PRODUCT_PACKAGES += \
     HotwordEnrollmentXGoogleHEXAGON_WIDEBAND \
     DeviceInfo \
     ImsRcsService \
+    QtiTelephony \
     QtiTelephonyService \
     atfwd \
     uceShimService \
     WfdService \
     dpmserviceapp \
+    ims \
     qcrilmsgtunnel \
     WfdCommon \
     tcmclient \
